89befedd7be88da6e2064c65d874603b063abab6
liuzhisheng <lzswxhm@163.com>
2012-08-09 15:01:11 +0800

    First commit

---
+++ src/SqlitePassword/SqlitePassword.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SqlitePassword
+{
+    partial class SqlitePassword
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxFile = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.tbxOldPwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxNewPwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnChangePwd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SQLite File：";
+            // 
+            // tbxFile
+            // 
+            this.tbxFile.Location = new System.Drawing.Point(108, 21);
+            this.tbxFile.Name = "tbxFile";
+            this.tbxFile.Size = new System.Drawing.Size(367, 21);
+            this.tbxFile.TabIndex = 1;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(481, 19);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(53, 23);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Select";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // tbxOldPwd
+            // 
+            this.tbxOldPwd.Location = new System.Drawing.Point(108, 57);
+            this.tbxOldPwd.Name = "tbxOldPwd";
+            this.tbxOldPwd.Size = new System.Drawing.Size(367, 21);
+            this.tbxOldPwd.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Old Password：";
+            // 
+            // tbxNewPwd
+            // 
+            this.tbxNewPwd.Location = new System.Drawing.Point(108, 97);
+            this.tbxNewPwd.Name = "tbxNewPwd";
+            this.tbxNewPwd.Size = new System.Drawing.Size(367, 21);
+            this.tbxNewPwd.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "New Password：";
+            // 
+            // btnChangePwd
+            // 
+            this.btnChangePwd.Location = new System.Drawing.Point(234, 145);
+            this.btnChangePwd.Name = "btnChangePwd";
+            this.btnChangePwd.Size = new System.Drawing.Size(125, 23);
+            this.btnChangePwd.TabIndex = 7;
+            this.btnChangePwd.Text = "Change Password";
+            this.btnChangePwd.UseVisualStyleBackColor = true;
+            this.btnChangePwd.Click += new System.EventHandler(this.btnChangePwd_Click);
+            // 
+            // SqlitePassword
+            // 
+            this.AcceptButton = this.btnChangePwd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 195);
+            this.Controls.Add(this.btnChangePwd);
+            this.Controls.Add(this.tbxNewPwd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxOldPwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.tbxFile);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "SqlitePassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change SQLite Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxFile;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox tbxOldPwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxNewPwd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnChangePwd;
+    }
+}
+
---
+++ src/SqlitePassword/SqlitePassword.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SqlitePassword
+{
+    public partial class SqlitePassword : Form
+    {
+        private SQLiteConnection conn;
+
+        public SqlitePassword()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        public void ChangePassword(string filePath, string oldPwd, string newPwd)
+        {
+            conn = new SQLiteConnection();
+            conn.ConnectionString = "Data Source=" + filePath;
+            if (!string.IsNullOrEmpty(oldPwd))
+            {
+                conn.ConnectionString += ";Password=" + oldPwd;
+            }
+            try
+            {
+                conn.Open();
+                conn.ChangePassword(newPwd);
+                MessageBox.Show("Password change successfully", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnChangePwd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbxFile.Text) || !File.Exists(tbxFile.Text))
+            {
+                MessageBox.Show("File not exist.", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            ChangePassword(tbxFile.Text, tbxOldPwd.Text, tbxNewPwd.Text);
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fileDlg = new OpenFileDialog())
+            {
+                if (fileDlg.ShowDialog() == DialogResult.OK)
+                {
+                    tbxFile.Text = fileDlg.FileName;
+                }
+            }
+        }
+    }
+}
